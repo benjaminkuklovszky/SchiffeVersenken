fb219d533edc7547a99dc04621fcbf2de9e39646
benjaminkuklovszky <benjamin.kuklovszky@gmail.com>
2018-03-06 14:02:42 +0100

    lel

--- src/team/schiffe/versenken/Main.java
+++ src/team/schiffe/versenken/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Schiffeversenken");
-			primaryStage.show();
+			primaryStage.show();//kommentar
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
