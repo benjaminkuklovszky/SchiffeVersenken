6ea293b7a8b8624d6aacc93ac29599ce8cb674ba
julian <julian@10.91.73.200>
2018-03-06 14:09:33 +0100

    kommentare weg!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/team/schiffe/versenken/Main.java
+++ src/team/schiffe/versenken/Main.java
@@ -15,9 +15,8 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Schiffeversenken");
-			primaryStage.show();//kommentar
-		} catch(Exception e) {	//bessere kommentar
-								//kjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+			primaryStage.show();
+		} catch(Exception e) {							
 			e.printStackTrace();
 		}
 	}
