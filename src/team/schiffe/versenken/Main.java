5e2a05bfd3de6ed8b167a85c4dc909a07fa87341
benjaminkuklovszky <benjamin.kuklovszky@gmail.com>
2018-03-06 14:50:01 +0100

    1

--- src/team/schiffe/versenken/Main.java
+++ src/team/schiffe/versenken/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("DasBessereSchiffeversenken_beschte");
+			primaryStage.setTitle("1BessereSchiffeversenken_beschte");
 
 			primaryStage.show();
 		} catch(Exception e) {
